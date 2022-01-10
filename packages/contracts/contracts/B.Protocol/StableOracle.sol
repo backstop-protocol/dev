37131edf983ea383972ead324fc7da4a17876b3e
yaron velner <yaron172@yahoo.com>
2022-01-10 08:14:17 +0200

    oracles

--- packages/contracts/contracts/B.Protocol/StableOracle.sol
+++ packages/contracts/contracts/B.Protocol/StableOracle.sol
@@ -29,3 +29,74 @@ contract StableOralce {
         timestamp = now; // override timestamp
     }
 }
+
+contract NonUSDOracle {
+    AggregatorV3Interface immutable public srcOracle;
+    AggregatorV3Interface immutable public targetOracle;
+
+    constructor(AggregatorV3Interface _srcOracle, AggregatorV3Interface _targetOracle) public {
+        srcOracle = _srcOracle;
+        targetOracle = _targetOracle;
+    }
+
+    function decimals() public view returns (uint8) {
+        return targetOracle.decimals();
+    }
+
+    function latestRoundData() public view
+        returns
+        (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 timestamp,
+            uint80 answeredInRound
+        )
+    {
+        int targetAnswer;
+        int srcAnswer;
+        uint targetTimestamp;
+        uint srcTimestamp;
+
+        (roundId, targetAnswer, startedAt, targetTimestamp, answeredInRound) = targetOracle.latestRoundData();
+        (, srcAnswer, , srcTimestamp,) = srcOracle.latestRoundData();
+
+        uint srcDecimals = srcOracle.decimals();
+
+        answer = targetAnswer * int(10 ** srcDecimals) / srcAnswer;
+        timestamp = srcTimestamp > targetTimestamp ? targetTimestamp : srcTimestamp; // take the minimum
+
+        // check if there was an overflow in calculation - if there was, return 0 timestamp and answer
+        bool overflow = false;
+        if(targetAnswer > type(int128).max) overflow = true;
+        if(srcDecimals > 18) overflow = true;
+
+        if(overflow) {
+            timestamp = 0;
+            answer = 0;
+        }
+    }
+}
+
+contract FixedOracle {
+    function decimals() public pure returns (uint8) {
+        return 8;
+    }
+
+    function latestRoundData() public view
+        returns
+        (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 timestamp,
+            uint80 answeredInRound
+        )
+    {
+        roundId = 5;
+        answer = 1e8;
+        startedAt = now;
+        timestamp = now;
+        answeredInRound = 5;
+   }
+}
