02da74e9a9d813502ce0be2f80c8c7c6a6584a33
yaron velner <yaron172@yahoo.com>
2022-01-11 19:45:02 +0200

    arb smart contract

--- packages/contracts/test/B.Protocol/ArbTest.js
+++ packages/contracts/test/B.Protocol/ArbTest.js
@@ -1,3 +1,4 @@
+const { artifacts } = require("hardhat")
 const deploymentHelper = require("./../../utils/deploymentHelpers.js")
 const testHelpers = require("./../../utils/testHelpers.js")
 const th = testHelpers.TestHelper
@@ -70,101 +71,69 @@ contract('BAMM', async accounts => {
     beforeEach(async () => {
     })
 
-    it("Arb", async () => {
+    it.only("Arb", async () => {
       const whale = "0x23cBF6d1b738423365c6930F075Ed6feEF7d14f3" // has eth and usdt
-
-      const arbAddress = "0xcEAF62Ba209e2FB7990D29c5f5157377D54FC7b2"
-      const keeperAddress = "0x102887d6bFC58B0abE721AAD1ce5A036ACe542c8"
 /*
       await hre.network.provider.request({
         method: "hardhat_impersonateAccount",
         params: [whale], 
-      })*/
+      })
 
-      const factory = await (artifacts.require("UniswapFactory.sol")).at("0x1F98431c8aD98523631AE4a59f267346ea31F984")      
-      console.log(await factory.getPool.call("0x82aF49447D8a07e3bd95BD0d56f35241523fBab1", "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8", 500))
-            
+     const cusdc = await MockToken.at("0x04068DA6C83AFCFA0e13ba15A6696662335D5B75")
+      const creamArb = await artifacts.require("CreamArb").new()
+      console.log("cream", creamArb.address)
 
-      const reserve = await (artifacts.require("UniswapReserve.sol")).at("0xC31E54c7a869B9FcBEcc14363CF510d1c41fa443")
+      //const isProf = await creamArb.checkProfitableArb.call(dec(1,17), 0, "0x6d62d6Af9b82CDfA3A7d16601DDbCF8970634d22", cusdc.address)
+      //console.log({isProf})
+       
 
+      console.log("sending usdc")
+      //await cusdc.transfer(creamArb.address, dec(3,6), {from: whale})
+*/
+      const keeper = await artifacts.require("BFantomKeeper").at("0xb5CDc43cefd1826A669Dbd3A8D6180a3B623aef7")//(creamArb.address, {from: whale})
 
-      console.log(await reserve.token0())
-      console.log(await reserve.token1())
+      const bammUsdc = "0xEDC7905a491fF335685e2F2F1552541705138A3D"
+      const bammDai = "0x6d62d6Af9b82CDfA3A7d16601DDbCF8970634d22"
+/*
+      console.log("setting min")
+      await keeper.setMinQty(dec(1,16), {from: whale})
+      console.log("setting max")
+      await keeper.setMaxQty(dec(100000,18), {from: whale})      
+      console.log("adding bamm usdc")
+      await keeper.addBamm(bammUsdc, {from: whale})
+      console.log("adding bamm dai")      
+      await keeper.addBamm(bammDai, {from: whale})
+*/
+      console.log("find qty")
+      console.log(keeper.address)
+      const result = await keeper.findSmallestQty.call({gas: 100000000})
+      console.log(result[0].toString())
+      console.log({result})
 
-      const arb = await Arb.at(arbAddress)
-      bamm = await BAMM.at("0x24099000AE45558Ce4D049ad46DDaaf71429b168")
+      console.log("check upkeep")
+      const up = await keeper.checkUpkeep.call("0x", {gas: 100000000})
+      console.log({up})
+      console.log("do upkeep")
+      console.log("before", (await cusdc.balanceOf(keeper.address)).toString())
+      await keeper.performUpkeep(up.performData)
+      console.log("after", (await cusdc.balanceOf(keeper.address)).toString())      
 
-      console.log("giving allowance")
-      //await arb.approve(bamm.address, {from: whale})
+      const res = await keeper.checker.call({gas: 100000000})
+      console.log({res})
 
-      
+      const bamm = "0xEDC7905a491fF335685e2F2F1552541705138A3D"
+      const dest = "0x29b0Da86e484E1C0029B56e817912d778aC0EC69"
+      const creamUSDC = "0x328A7b4d538A2b3942653a9983fdA3C12c571141"
 
+      /*
+      console.log("whale", (await cusdc.balanceOf(whale)).toString())      
+      await creamArb.arb(bamm, creamUSDC, dec(1,5), dest, {from: whale})
 
-      //const arbChecker = await (artifacts.require("ArbChecker.sol")).new(arb.address,{from:whale})
-
-      const bKeeperImpl = await (artifacts.require("BKeeper.sol")).at(keeperAddress)
-      //const proxy = await (artifacts.require("KeeperProxy.sol")).new(bKeeperImpl.address, {from:whale})
-      //const gelato = await (artifacts.require("BGelato.sol")).new(proxy.address, {from:whale})
-      const bKeeper = bKeeperImpl //await (artifacts.require("BKeeper.sol")).at(proxy.address)
-
-      
-
-      //await bKeeper.initParams(dec(1,18), "0", 100, {from: whale})
-
-      //await bKeeper.setMinEthQty(1, {from: whale})
-      //await bKeeper.setMaxEthQty(dec(1,18), {from: whale})            
-      //await bKeeper.addBamm(bamm.address, {from: whale})      
-      console.log("checker()")
-      const retVal = await bKeeper.checker.call(/*{gas:100000000}*/)
-      console.log(retVal[0].toString())
-      console.log(retVal[1].toString())
-      console.log((await web3.eth.getBalance(bKeeper.address)).toString())
-      await web3.eth.sendTransaction({from: whale, to: bKeeper.address, data: retVal[1]})       
-      //await gelato.doer(retVal[1], {from: whale})
-      console.log((await web3.eth.getBalance(bKeeper.address)).toString())            
-      return
-
-
-      console.log((await web3.eth.getBalance(arbChecker.address)).toString())      
-      await arbChecker.checkProfitableArb("228664402009608",1, bamm.address)
-      console.log((await web3.eth.getBalance(arbChecker.address)).toString())
-
-      return
-      
-      
-
-
-      console.log("checking bamm price")
-      console.log((await bamm.getSwapEthAmount(dec(1,6))).toString())
-
-      console.log(arb.address)
-      console.log(await arb.LENS())      
-      console.log((await arb.getPrice.call(dec(1,18))).toString())
-
-      console.log("do arbtirage")
-      console.log("trying to arb")
-      console.log((await web3.eth.getBalance(arb.address)).toString())
-      await arb.swap("228664402009608", bamm.address)
-      console.log((await web3.eth.getBalance(arb.address)).toString())
+      console.log((await cusdc.balanceOf(creamArb.address)).toString())
+      console.log("whale", (await cusdc.balanceOf(whale)).toString())      
+      //const tx = await creamArb.f()
+     */
     })
-
-
-    // tests:
-    // 1. complex lqty staking + share V
-    // 2. share test with ether V
-    // 3. basic share with liquidation (withdraw after liquidation) V
-    // 4. price that exceeds max discount V
-    // 5. price that exceeds balance V
-    // 5.5 test fees and return V
-    // 5.6 test swap  v
-    // 6.1 test fetch price V
-    // 6. set params V
-    // 7. test with front end v
-    // 8. formula V
-    // 9. lp token - transfer sad test
-    // 11. pickle V
-    // 10. cleanups - compilation warnings. cropjoin - revoke changes and maybe make internal. V
-    // 12 - linter. events
   })
 })
 
