8b6a283edd36e172b95bd5be29ffd5843fb74bd7
yaron velner <yaron172@yahoo.com>
2022-04-03 16:13:37 +0300

    yml for liquidation bot

--- packages/liquidator/handler.js
+++ packages/liquidator/handler.js
@@ -1,5 +1,6 @@
 'use strict';
 const {runOnLambda} = require('./liquidator')
+const {runOnLambda: runOnLambdaPolygon} = require('./liquidator_generic')
 const {runBKeeper} = require('./bKeeper')
 
 module.exports.liquidate = async (event) => {
@@ -10,6 +11,36 @@ module.exports.liquidate = async (event) => {
       body: JSON.stringify(
         {
           message: 'liquidator runOnLambda executed successfully!',
+          input: event,
+        },
+        null,
+        2
+      ),
+    };
+  } catch (err){
+    console.error(err)
+    return {
+      statusCode: 500,
+      body: JSON.stringify(
+        {
+          message: err.message,
+          input: event,
+        },
+        null,
+        2
+      ),
+    };
+  }
+};
+
+module.exports.liquidate_polygon = async (event) => {
+  try{
+  await runOnLambdaPolygon()
+  return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          message: 'liquidator runOnLambdaPolygon executed successfully!',
           input: event,
         },
         null,
